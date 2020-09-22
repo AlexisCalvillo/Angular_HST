e032beee5dabff7db75b6f00999d2926ca146e3a
Alexis Calvillo <acalvillm@gmail.com>
2020-09-21 20:07:46 -0500

    Generación de componentes para mostrar menú

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 //Importar módulos de los componentes instalados en la configuración
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
 import {FlexLayoutModule} from '@angular/flex-layout';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
